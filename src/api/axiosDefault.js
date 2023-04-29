85fc2b4602b8c8a7cb818e2647ff101febd3f290
NatasaMiric <natasa.miric94@gmail.com>
2023-04-29 10:55:17 +0000

    feat: Add sign up form that will create users account and add custom css

---
+++ src/api/axiosDefault.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+
+// this code is based on Code Institute walkthrough project Moments
+
+axios.defaults.baseURL = 'https://feedme-api.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
