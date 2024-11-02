e0418c889e8800b3e963c89ab4b5deb33d270960
NatasaMiric <natasa.miric94@gmail.com>
2024-11-02 22:57:21 +0000

    Prepare for deployment to Render

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // this code is based on Code Institute walkthrough project Moments
 
-axios.defaults.baseURL = 'https://feedme-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://feedme-api-h1j3.onrender.com';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
